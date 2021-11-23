f045cc01c9d0002851134474d2826a618b3814d9
Clas_RCDM <raphaelcalixto2013@gmail.com>
2021-11-23 14:12:49 -0400

    exercício 11

---
+++ Exercícios/exe_011.py
@@ -0,0 +1,6 @@
+print('{0} Desafio 11 {0}'.format('=' * 10))
+lar = float(input('Largura: '))
+alt = float(input('Altura: '))
+area = lar * alt
+print('A área é {:.2f}, precisa de {:.2f} litros de tinta'.format(area,
+                                                          area / 2))
