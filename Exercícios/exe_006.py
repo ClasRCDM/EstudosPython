d6d80ccb54d20ac57d1a81e37290fdd4b861a3bd
Clas_RCDM <raphaelcalixto2013@gmail.com>
2021-11-23 13:45:11 -0400

    exercício 6

--- Exercícios/exe_006.py
+++ Exercícios/exe_006.py
@@ -1,2 +1,5 @@
-print('{0} Desafio  06 {0}'.format('=' * 10))
-
+print('{0} Desafio 06 {0}'.format('=' * 10))
+number = int(input('Digite um número: '))
+print('O dobro de {} é: {}'.format(number, number * 2))
+print('O triplo de {} é: {}'.format(number, number * 3))
+print('A raiz quadrada de {} é: {:.2f}'.format(number, number ** (1/2)))
