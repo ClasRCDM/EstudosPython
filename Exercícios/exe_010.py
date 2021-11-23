7c2b55489100812829d6ff24781e4c23dcc589ed
Clas_RCDM <raphaelcalixto2013@gmail.com>
2021-11-23 14:07:41 -0400

    exercício 10

---
+++ Exercícios/exe_010.py
@@ -0,0 +1,4 @@
+print('{0} Desafio 10 {0}'.format('=' * 10))
+dinheiro = float(input('Quantos R$ você tem? '))
+print('Com R${} reais você consegue {} dolares'.format(dinheiro,
+                                                       3.27 * dinheiro))
