1ee25ce9c0b7e6576c206782b3a4b0492b8cb4c3
Clas_RCDM <raphaelcalixto2013@gmail.com>
2021-11-29 15:20:32 -0400

    exe 28

--- Exercícios/exe_028.py
+++ Exercícios/exe_028.py
@@ -1,1 +1,12 @@
+from random import randint
 
+print('{0} Desafio {0}'.format('=' * 10))
+
+IA = randint(0, 5)
+
+jogador = int(input('Descubra qual numero a IA jogou: [0 a 5] : '))
+
+if jogador == IA:
+    print('Você acertou PARÁBENS!')
+else:
+    print('I você errooouuu!!!')
