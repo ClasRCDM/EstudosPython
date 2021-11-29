fea88c9245cf11a12616932a62411216b10c9165
Clas_RCDM <raphaelcalixto2013@gmail.com>
2021-11-29 15:28:56 -0400

    exe 30

---
+++ Exercícios/exe_030.py
@@ -0,0 +1,10 @@
+print('{0} Desafio 30 {0}'.format('=' * 10))
+
+numero = int(input('Um numero inteiro: '))
+
+resultado = numero % 2
+
+if resultado == 0:
+    print('O numero é PAR!')
+else:
+    print('O numero é IMPAR')
