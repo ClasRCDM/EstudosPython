98c7a3c51781e9568c9a1d71b85838fa3483303c
Clas_RCDM <raphaelcalixto2013@gmail.com>
2021-11-30 15:55:02 -0400

    exe 37

--- Exercícios/exe_037.py
+++ Exercícios/exe_037.py
@@ -1,1 +1,13 @@
-print('{1}{0} Desafio 37 {0}'.format('=' * 10, '\033[4;33m'))
+print('{1}{0} Desafio 37 {0}{2}'.format('=' * 10, '\033[4;33m', '\033[m'))
+
+numero = int(input('Um numero para converter: \033[32m'))
+
+print('\033[31mEscolha entre Binário, Octal e Hexadecimal\033[m')
+conversao = str(input('Qual: \033[32m')).strip().title()
+
+if conversao in 'Binário Binario':
+    print('{}, Se torna {}'.format(numero, binario))
+elif conversao == 'Octal':
+    pass
+elif conversao == 'Hexadecimal':
+    pass
