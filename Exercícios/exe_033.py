cb37102a7d7504a5c9c7a433e16f54d71ab8e036
Clas_RCDM <raphaelcalixto2013@gmail.com>
2021-11-30 09:52:20 -0400

    exe 33

--- Exercícios/exe_033.py
+++ Exercícios/exe_033.py
@@ -4,21 +4,17 @@ numero1 = int(input('Primeiro: '))
 numero2 = int(input('Segundo: '))
 numero3 = int(input('Terceiro: '))
 
-maior = 0
-menor = 0
+maior = numero1
+menor = numero1
 
-if numero1 > numero2:
-    maior = numero1
-if numero2 > numero3:
-    maior = numero2
-if numero3 > numero1:
-    maior = numero3
-
-if numero1 < numero2:
-    menor = numero1
-if numero2 < numero3:
+if numero2 < numero1 and numero2 < numero3:
     menor = numero2
-if numero3 < numero1:
+if numero3 < numero1 and numero3 < numero2:
     menor = numero3
+
+if numero2 > numero1 and numero2 > numero3:
+    maior = numero2
+if numero3 > numero1 and numero3 > numero2:
+    maior = numero3
 
 print('O maior numero é {}\nO menor numero é {}'.format(maior, menor))
