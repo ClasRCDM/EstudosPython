674a6b6b95bbe342ff7b0e9dd08a5713a1fae1de
Clas_RCDM <raphaelcalixto2013@gmail.com>
2021-11-30 09:34:43 -0400

    exe 28

--- Exercícios/exe_028.py
+++ Exercícios/exe_028.py
@@ -1,12 +1,21 @@
 from random import randint
+from time import sleep
 
-print('{0} Desafio {0}'.format('=' * 10))
+print('{0} Desafio 28 {0}'.format('=' * 10))
 
 IA = randint(0, 5)
 
+print('-=-' * 20)
+print('Vou pensar em um número entre 0 e 5. Tente adivinhar...')
+print('-=-' * 20)
+
 jogador = int(input('Descubra qual numero a IA jogou: [0 a 5] : '))
+
+print('PROCESSANDO...')
+sleep(2)
 
 if jogador == IA:
     print('Você acertou PARABÉNS!')
 else:
     print('I você errooouuu!!!')
+    print('GANHEI!! Eu pensei no número {}'.format(IA))
