4d10c7e725d816842bf30494696b79a66efb51db
Clas_RCDM <raphaelcalixto2013@gmail.com>
2021-11-30 09:41:59 -0400

    exe 31

--- Exercícios/exe_031.py
+++ Exercícios/exe_031.py
@@ -2,9 +2,13 @@ print('{0} Desafio 31 {0}'.format('=' * 10))
 
 distancia = float(input('Distancia da viagem: '))
 
-if distancia <= 200:
+print('Você está prestes a começar uma viagem de {}Km'.format(distancia))
+
+'''if distancia <= 200:
     valor = distancia * 0.50
 else:
-    valor = distancia * 0.45
+    valor = distancia * 0.45'''
 
-print('O preço da viagem vai ser {}'.format(valor))
+valor = distancia * 0.5 if distancia <= 200 else distancia * 0.45
+
+print('O preço da viagem vai ser R${:.2f}'.format(valor))
