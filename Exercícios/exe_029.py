2eb9ddbd1755bd089e521041aac6fc5eff393a3b
Clas_RCDM <raphaelcalixto2013@gmail.com>
2021-11-30 09:38:01 -0400

    exe 29

--- Exercícios/exe_029.py
+++ Exercícios/exe_029.py
@@ -5,6 +5,7 @@ velocidade = float(input('Velocidade do carro: '))
 if velocidade > 80:
     v = (velocidade - 80) * 7
 
-    print('Você foi mutado OTARIOO!!!\nVocê ira pagar R${}'.format(v))
+    print('Você excedeu o limite permitido de 80Km/h')
+    print('Você foi mutado OTARIOO!!!\nVocê ira pagar R${:.2f}'.format(v))
 else:
     print('Segui uma boa viagem')
