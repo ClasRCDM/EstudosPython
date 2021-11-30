b67585cc24df6df3d25e3c1095d3af29bbbb69e1
Clas_RCDM <raphaelcalixto2013@gmail.com>
2021-11-30 10:37:58 -0400

    aula 11

---
+++ Mundo 1/Aula11.py
@@ -0,0 +1,8 @@
+print('\033[4;33mOlá, Mundo!!\033[m')
+
+a, b = 5, 3
+
+print('Os valores são \033[33m{}\033[m e \033[31m{}\033[m!!!'.format(a, b))
+
+nome = 'Raphael'
+print('Olá Muito prazer {}{}{}!!!'.format('\033[36m', nome, '\033[m'))
