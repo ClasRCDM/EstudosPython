b1f95291ef00f5cd1389279085cfa68eec7e50de
Clas_RCDM <raphaelcalixto2013@gmail.com>
2021-12-01 12:37:30 -0400

    exe 42

--- Exercícios/exe_040.py
+++ Exercícios/exe_040.py
@@ -3,7 +3,7 @@ print('{1}{0} Desafio 40 {0}{2}'.format('=' * 10, '\033[4;33m', '\033[m'))
 nota1 = float(input('Primeira nota: \033[32m'))
 nota2 = float(input('\033[mSegunda nota: \033[32m'))
 
-nota_final = nota1 + nota2 / 2
+nota_final = (nota1 + nota2) / 2
 
 print('\033[34mSua nota é {}'.format(nota_final))
 
--- Exercícios/exe_042.py
+++ Exercícios/exe_042.py
@@ -1,1 +1,16 @@
 print('{1}{0} Desafio 42 {0}{2}'.format('=' * 10, '\033[4;33m', '\033[m'))
+
+r1 = float(input('Primeiro segmento: '))
+r2 = float(input('Segundo segmento: '))
+r3 = float(input('Terceiro segmento: '))
+
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('Os segmentos acima PODEM FORMAR um triângulo!')
+    if r1 == r2 == r3:
+        print('EQUILÁTERO!')
+    elif r1 != r2 != r3 != r1:
+        print('ESCALENO')
+    else:
+        print('ISÓSCELES')
+else:
+    print('Os segmentos acima NÃO PODEM FORMAR um triângulo')
