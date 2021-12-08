b76fa4ff5885a0c8e3de5dbb31f6dbb44b4f76a0
Clas_RCDM <raphaelcalixto2013@gmail.com>
2021-12-08 16:55:08 -0400

    exe 47

--- Exercícios/exe_047.py
+++ Exercícios/exe_047.py
@@ -1,6 +1,6 @@
 print('{1}{0} Desafio 45 {0}{2}'.format('=' * 10, '\033[4;33m', '\033[m'))
 
-print('{}PARES{}'.format('\031[4;33m', '\033[m'))
+print('{}PARES{}'.format('\033[4;31m', '\033[m'))
 for pares in range(0, 50):
     if pares // 2 == 0:
-        print('{}{}{}'.format('\032[4;33m', pares, '\033[m'))
+        print('{}{}{}'.format('\033[4;35m', pares, '\033[m'))
