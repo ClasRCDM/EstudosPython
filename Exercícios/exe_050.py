331b3040215d8a2018ab7ec05a0104169b8b357e
Clas_RCDM <raphaelcalixto2013@gmail.com>
2021-12-08 17:22:29 -0400

    exe 50

--- Exercícios/exe_050.py
+++ Exercícios/exe_050.py
@@ -1,1 +1,14 @@
-print('{1}{0} Desafio 45 {0}{2}'.format('=' * 10, '\033[4;33m', '\033[m'))
+print('{1}{0} Desafio 50 {0}{2}'.format('=' * 10, '\033[4;33m', '\033[m'))
+
+s = 0
+números_descartados = list()
+
+for números in range(7):
+    n = int(input('Dígite número: '))
+    if n % 2 == 0:
+        s += n
+    else:
+        números_descartados.append(n)
+else:
+    print('A soma é {}'.format(s))
+    print('Os numeros descartados foram \n{}'.format(números_descartados))
