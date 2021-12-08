1024556126c617478c5b4d0616561a160d1fd41b
Clas_RCDM <raphaelcalixto2013@gmail.com>
2021-12-08 17:04:57 -0400

    exe 48

--- Exercícios/exe_048.py
+++ Exercícios/exe_048.py
@@ -1,1 +1,6 @@
 print('{1}{0} Desafio 45 {0}{2}'.format('=' * 10, '\033[4;33m', '\033[m'))
+
+print('{}IMPARES múltiplos de três{}'.format('\033[4;31m', '\033[m'))
+for pares in range(1, 501):
+    if pares % 2 != 0 and pares % 3 == 0:
+        print('{}{}{}'.format('\033[4;35m', pares, '\033[m'), end=' ')
