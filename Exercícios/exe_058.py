017baabb25181afd81b3991e20cc7b5166b5aa00
Clas_RCDM <raphaelcalixto2013@gmail.com>
2022-01-05 15:32:01 -0400

    exercicío 58

--- Exercícios/exe_058.py
+++ Exercícios/exe_058.py
@@ -1,1 +1,15 @@
+from random import randint
+
 print('{1}{0} Desafio 58 {0}{2}'.format('=' * 10, '\033[4;33m', '\033[m'))
+
+IA = randint(0, 10)
+
+tentativas = 0
+
+num = int(input('Tente adivinhar [0/10] '))
+while num != IA:
+    print('Você errou, tente novamente - [0/10]!!!')
+    tentativas += 1
+    num = int(input('Tente adivinhar [0/10] '))
+else:
+    print(f'Você acertou!!, Com {tentativas} tentativas.')
