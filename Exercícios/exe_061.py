d32e1733b4df38375f470b108c32480a2b6c1826
Clas_RCDM <raphaelcalixto2013@gmail.com>
2022-01-06 16:12:56 -0400

    exercicío 61

--- Exercícios/exe_061.py
+++ Exercícios/exe_061.py
@@ -1,1 +1,15 @@
 print('{1}{0} Desafio 61 {0}{2}'.format('=' * 10, '\033[4;33m', '\033[m'))
+
+prim = int(input('Primeiro termo: '))
+razão = int(input('Razão de PA: '))
+
+termo = prim
+cont = 1
+
+while cont <= 10:
+    print(f'{termo} > ', end='')
+
+    termo += razão
+    cont += 1
+else:
+    print('FIM')
