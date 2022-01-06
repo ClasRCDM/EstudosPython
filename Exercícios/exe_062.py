0d15e2610f7ed585310b17afbb275be5e3f10a84
Clas_RCDM <raphaelcalixto2013@gmail.com>
2022-01-06 16:22:04 -0400

    exercicío 62

--- Exercícios/exe_062.py
+++ Exercícios/exe_062.py
@@ -1,1 +1,23 @@
 print('{1}{0} Desafio 62 {0}{2}'.format('=' * 10, '\033[4;33m', '\033[m'))
+
+prim = int(input('Primeiro termo: '))
+razão = int(input('Razão de PA: '))
+
+termo = prim
+cont = 1
+
+total = 0
+mais = 10
+
+while mais != 0:
+    total += mais
+    while cont <= total:
+        print(f'{termo} > ', end='')
+
+        termo += razão
+        cont += 1
+    else:
+        print('PAUSA')
+        mais = int(input('\nQuantos termos você quer mostrar a mais? '))
+else:
+    print('FIM!!!')
