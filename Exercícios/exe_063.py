22c53783699ba5c935a1528e2afff4c95a8cbb30
Clas_RCDM <raphaelcalixto2013@gmail.com>
2022-01-06 16:31:52 -0400

    exercicío 63

--- Exercícios/exe_063.py
+++ Exercícios/exe_063.py
@@ -1,1 +1,20 @@
 print('{1}{0} Desafio 63 {0}{2}'.format('=' * 10, '\033[4;33m', '\033[m'))
+print('Sequencia de Fibonacci.\n')
+
+N = int(input('Quantos termos você quer mostrar? '))
+
+t1, t2 = 0, 1
+
+print(f'{t1} > {t2}', end='')
+cont = 3
+
+while cont <= N:
+    t3 = t1 + t2
+    print(f' > {t3}', end='')
+
+    t1 = t2
+    t2 = t3
+
+    cont += 1
+else:
+    print(' > FIM!')
