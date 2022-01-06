2741f674a6296ecd1ad665ed5217a7a941189c96
Clas_RCDM <raphaelcalixto2013@gmail.com>
2022-01-06 16:43:30 -0400

    exercicío 65

--- Exercícios/exe_065.py
+++ Exercícios/exe_065.py
@@ -1,1 +1,24 @@
 print('{1}{0} Desafio 65 {0}{2}'.format('=' * 10, '\033[4;33m', '\033[m'))
+
+resp = 'S'
+soma = quant = média = maior = menor = 0
+
+while resp in 'Ss':
+    num = int(input('Digite um número: '))
+
+    soma += num
+    quant += 1
+
+    if quant == 1:
+        maior = menor = num
+    else:
+        if num > maior:
+            maior = num
+        elif num < menor:
+            menor = num
+    resp = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
+else:
+    media = soma / quant
+
+    print(f'Você digitou {quant} números e a média foi {media}')
+    print(f'O maior valor foi {maior} e o menor foi {menor}')
