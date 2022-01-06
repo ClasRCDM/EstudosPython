d7f139b69414a61cb242a15319ff2e345e98dd91
Clas_RCDM <raphaelcalixto2013@gmail.com>
2022-01-06 16:38:12 -0400

    exercicío 64

--- Exercícios/exe_064.py
+++ Exercícios/exe_064.py
@@ -1,1 +1,12 @@
 print('{1}{0} Desafio 64 {0}{2}'.format('=' * 10, '\033[4;33m', '\033[m'))
+
+num = cont = soma = 0
+num = int(input('Dígite um número [999 para parar]: '))
+
+while num != 999:
+    soma += num
+    cont += 1
+
+    num = int(input('Dígite um número [999 para parar]: '))
+else:
+    print(f'Você digitou {cont} números e a soma entre eles foi {soma}.')
