bf2c9a3a09ee6808cd64c85f203f1b59e4549b5f
Clas_RCDM <raphaelcalixto2013@gmail.com>
2022-01-10 21:44:44 -0400

    exercício 67

--- Exercícios/exe_067.py
+++ Exercícios/exe_067.py
@@ -1,1 +1,12 @@
 print('{1}{0}{2}'.format(f'{" Exercício 67 ":=^25}', '\033[4;33m', '\033[m'))
+
+while True:
+    val = int(
+        input('[Para parar, digitar um número negativo!]\nQuer ver a tabuada de qual valor? '))
+
+    if val < 0: break
+
+    for tab in range(1, 11):
+        print(f'{tab} x {val:2} = {val*tab}')
+
+print('FIM!!!')
