12caf733d37cb583ab7e9d5aa8fb60dce41823af
Clas_RCDM <raphaelcalixto2013@gmail.com>
2022-01-10 20:58:26 -0400

    Aula 15

---
+++ Mundo 2/Aula15.py
@@ -0,0 +1,13 @@
+print('{1}{0} Aula 15 {0}{2}'.format('=' * 10, '\033[4;33m', '\033[m'))
+
+cont = 1
+while True:
+    print(f'{cont} ->', end=' ')
+    cont += 1
+    if cont >= 20:
+        break
+    if cont >= 10:
+        continue
+else:
+    print('oi')
+print('oi break')
