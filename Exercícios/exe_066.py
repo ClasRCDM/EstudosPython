183a90eac8d08f1b1dc10bed5ea73aa5e1864804
Clas_RCDM <raphaelcalixto2013@gmail.com>
2022-01-10 21:36:35 -0400

    exercício 67

--- Exercícios/exe_066.py
+++ Exercícios/exe_066.py
@@ -1,1 +1,13 @@
 print('{1}{0}{2}'.format(f'{" Exercício 66 ":=^25}', '\033[4;33m', '\033[m'))
+
+quant = soma = int(0)
+
+while True:
+    number = int(input('Digite um número: '))
+
+    if number == 999:break
+
+    quant += 1
+    soma += number
+
+print(f'Você digitou {quant} números e a soma dos mesmo é {soma}!')
