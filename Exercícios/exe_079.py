9ba9e2189ab029d869d431483355a7a654293894
Clas_RCDM <raphaelcalixto2013@gmail.com>
2022-01-17 11:16:05 -0400

    exercício 79

---
+++ Exercícios/exe_079.py
@@ -0,0 +1,17 @@
+print('{1}{0}{2}{3}'.format(f'{"_ Exercício 79 _":=^25}',
+      '\033[4;33m<< ', ' >>\033[m', '\n'))
+
+lista = list()
+
+while True:
+    valor = int(input('Digite um valor: '))
+    if valor not in lista:
+        print('Valor adicionado com sucesso...')
+        lista.append(valor)
+    else: print('Valor duplicado! Não adicionado...')
+
+    if str(input('Quer continuar? [S/N] ')).strip()[0] in 'Ss': continue
+    else: break
+
+print('-=' * 15)
+print('Você digitou os valores:', ' - '.join([str(valores) for valores in lista]))
