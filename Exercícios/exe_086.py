80588d56718b6cf530a1927e672e7ce8051b2496
Clas_RCDM <raphaelcalixto2013@gmail.com>
2022-01-18 08:31:57 -0400

    exercício 86

---
+++ Exercícios/exe_086.py
@@ -0,0 +1,13 @@
+print('{1}{0}{2}{3}'.format(f'{"_ Exercício 86 _":=^25}',
+      '\033[4;33m<< ', ' >>\033[m', '\n'))
+
+lista = list(([0, 0, 0], [0, 0, 0], [0, 0, 0]))
+
+for x in range(len(lista)):
+    for y in range(len(lista)):
+        lista[x][y] = int(input(f'Digite um valor para [{x}, {y}]: '))
+else:
+    for x in range(len(lista)):
+        for y in range(len(lista)):
+            print(f'[  {lista[x][y]}  ]', end='')
+        print()
