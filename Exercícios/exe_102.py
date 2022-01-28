5948675615245364a1419a381c8a58c6da2be50e
Clas_RCDM <raphaelcalixto2013@gmail.com>
2022-01-28 09:41:13 -0400

    exercício 102

--- Exercícios/exe_102.py
+++ Exercícios/exe_102.py
@@ -3,9 +3,17 @@ print('{1}{0}{2}{3}'.format(f'{"_ Exercício 102 _":=^25}',
 
 
 def fatorial(n, show=False):
+    """
+    -> Calcula o Fatorial de um número.
+    :param n : O número a ser calculado.
+    :param show: (opcional) Mostrar ou não a conta.
+    :return: O valor do Fatorial de um número n.
+    """
+    
     f = 1
     for c in range(n, 0, -1):
         if show:
+            print(c, end='')
             if c > 1: print(' x ', end='')
             else: print(' = ', end='')
         f *= c
@@ -14,4 +22,5 @@ def fatorial(n, show=False):
 
 # -
 
-
+print(fatorial(5, show=True))
+print(fatorial(5))
